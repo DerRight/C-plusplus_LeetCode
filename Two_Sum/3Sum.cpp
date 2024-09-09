9f707fdf95d01eb2462a916543ef2e7cf9333f97
DerRight <zxc123456789123456@gmail.com>
2024-09-09 18:36:46 +0800

    新增3Sum的片段程式碼

---
+++ Two_Sum/3Sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<vector<int>> Solution_3Sum(vector<int>& nums) {
+	unordered_map<int, int> numMap;
+
+	for (int i = 0; i < nums.size(); i++) {
+		int complement;
+	}
+}
+
+int main() {
+	vector<int> nums = { -1, 0, 1, 2, -1,-4 };
+	Solution_3Sum(nums);
+
+	return 0;
+}
