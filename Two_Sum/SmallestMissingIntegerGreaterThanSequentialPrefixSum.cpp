d04481c022b46b76e6c286eea6f6439a50254a2f
DerRight <zxc123456789123456@gmail.com>
2024-09-11 17:26:36 +0800

    新增了完整程式碼

--- Two_Sum/SmallestMissingIntegerGreaterThanSequentialPrefixSum.cpp
+++ Two_Sum/SmallestMissingIntegerGreaterThanSequentialPrefixSum.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
+#include <bitset>
 using namespace std;
 
 int missingInteger(vector<int>& nums) {
-	int currentPrefixSum = 0;
-	unordered_set<int> prefixSums;
+	// 初始設定首碼和為陣列的第一個元素
+	int s = nums[0];
 
-	for (int num : nums) {
-		currentPrefixSum += num;
-		prefixSums.insert(currentPrefixSum);
+	// 遍歷整個組數，計算從第二個元素開始的首碼和
+	// 條件是當前元素等於前一個元素 +1 (即陣列連續)
+	for (int j = 1; j < nums.size() && nums[j] == nums[j - 1] + 1; ++j) {
+		s += nums[j];
 	}
 
-	int currentMin = 1;
-	while (prefixSums.find(currentMin) != prefixSums.end()) {
-		++currentMin;
+	// 建立一個 bitset (長度為51) 來標記陣列中已經出現的整數
+	bitset<51> vis;
+
+	// 遍歷數組，將每個元素在 bitset 中標記一個 1 ，表示已經出現
+	for (int x : nums) {
+		vis[x] = 1;
 	}
 
-	return currentMin;
+	// 從首碼和 s 開始遍歷，尋找第一個未出現在陣列中的整數
+	for (int x = s;; ++x) {
+
+		// 如果 x 超過 bitset 的範圍 (即51)，或者 x 沒有在陣列中出現 (vis[x]為0)，則返回 x
+		if (x >= 51 || !vis[x]) {
+			return x; // 返回最小的缺失整數值
+		}
+	}
 }
 
 int main() {
